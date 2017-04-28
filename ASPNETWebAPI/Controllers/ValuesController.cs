1458dba4cf7e80b85a836350eb8a2f7fddaca6da
wanasak <u510610433@gmail.com>
2017-04-28 13:06:44 +0700

    http get post put delete

--- ASPNETWebAPI/Controllers/ValuesController.cs
+++ ASPNETWebAPI/Controllers/ValuesController.cs
@@ -9,31 +9,39 @@ namespace ASPNETWebAPI.Controllers
 {
     public class ValuesController : ApiController
     {
+        static List<string> fruits = new List<string>()
+        {
+            "Banana", "Apple", "Orange"
+        };
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return fruits;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return fruits[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            fruits.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            fruits[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            fruits.RemoveAt(id);
         }
     }
 }
