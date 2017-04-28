dbd46907df752ae75e22d14b508676f1e7464c71
wanasak <u510610433@gmail.com>
2017-04-28 13:12:00 +0700

    web api and sql server

---
+++ ASPNETWebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNETWebAPI.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<tblEmployee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.tblEmployees.ToList();
+            }
+        }
+
+        public tblEmployee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.tblEmployees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
