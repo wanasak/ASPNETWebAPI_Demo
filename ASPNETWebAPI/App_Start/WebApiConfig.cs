00d933e59a23b83a8367e4c35c9ff4d54f3f99ac
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-04-29 01:30:42 -0700

    web api content negotiation

--- ASPNETWebAPI/App_Start/WebApiConfig.cs
+++ ASPNETWebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,11 @@ namespace ASPNETWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Change JSON data to be indented
+           config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            // Change JSON data to be camel case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
