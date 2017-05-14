b82e3b3be9523e7f41d5a67661126ffae3b85a1d
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-05-14 13:55:28 +0700

    aspnet web api token authen

---
+++ ASPNETWebAPI_Token/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNETWebAPI_Token.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDB_WebAPITokenBaseAuthenEntities entities = new EmployeeDB_WebAPITokenBaseAuthenEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
---
+++ ASPNETWebAPI_Token/SQL/initDatabase.sql
@@ -0,0 +1,22 @@
+﻿create database EmployeeDB_WebAPITokenBaseAuthen
+go
+
+use EmployeeDB_WebAPITokenBaseAuthen
+go 
+
+create table Employees
+(
+	ID int primary key identity,
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	Gender nvarchar(10),
+	Salary int
+)
+Insert into Employees values ('Mark', 'Hastings', 'Male', 60000)
+Insert into Employees values ('Steve', 'Pound', 'Male', 45000)
+Insert into Employees values ('Ben', 'Hoskins', 'Male', 70000)
+Insert into Employees values ('Philip', 'Hastings', 'Male', 45000)
+Insert into Employees values ('Mary', 'Lambeth', 'Female', 30000)
+Insert into Employees values ('Valarie', 'Vikings', 'Female', 35000)
+Insert into Employees values ('John', 'Stanmore', 'Male', 80000)
+go
