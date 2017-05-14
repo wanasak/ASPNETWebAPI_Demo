53abb10c44fb7489f270a3706aedb2fb758f1079
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-05-14 15:42:32 +0700

    attribute routing

---
+++ ASPNETWebAPI_Token/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETWebAPI_Token.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNETWebAPI_Token.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student() { ID = 1, Name = "Tom" },
+            new Student() { ID = 2, Name = "Sam" },
+            new Student() { ID = 3, Name = "John" }
+        };
+
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.ID == id);
+        }
+
+        [Route("api/students/{id}/courses")]
+        public IEnumerable<string> GetStudentCourses(int id)
+        {
+            if (id == 1)
+                return new List<string>() { "C#", "ASP.NET", "SQL Server" };
+            else if (id == 2)
+                return new List<string>() { "ASP.NET Web API", "C#", "SQL Server" };
+            else
+                return new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+        }
+    }
+}
