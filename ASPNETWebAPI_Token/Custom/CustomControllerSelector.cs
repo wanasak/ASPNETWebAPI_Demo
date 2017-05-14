70b8c227c60de938bc5f3356b7c9194e713cc823
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-05-14 19:52:16 +0700

    version using custom header

--- ASPNETWebAPI_Token/Custom/CustomControllerSelector.cs
+++ ASPNETWebAPI_Token/Custom/CustomControllerSelector.cs
@@ -28,9 +28,16 @@ namespace ASPNETWebAPI_Token.Custom
             var controllerName = routeData.Values["controller"].ToString();
 
             string versionNumber = "1";
-            var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            if (versionQueryString["v"] != null)
-                versionNumber = versionQueryString["v"];
+            //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
+            //if (versionQueryString["v"] != null)
+            //    versionNumber = versionQueryString["v"];
+
+            // Get version number from custom header
+            string customHeader = "X-StudentService-Version";
+            if (request.Headers.Contains(customHeader))
+            {
+                versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            }
 
             controllerName = versionNumber == "1" ? controllerName : controllerName + "V2";
 
